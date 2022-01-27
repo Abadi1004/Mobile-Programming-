4c9131ff9d95d6f6c217aa739cdf72f813ce4b67
Abadi1004 <58854049+Abadi1004@users.noreply.github.com>
2022-01-27 12:26:31 +0300

    Add files via upload

---
+++ TravelRecordApp/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TravelRecordApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            var assembly = typeof(MainPage);
+            iconeImage.Source = ImageSource.FromResource("TravelRecordApp.images.Plogo1.png", assembly);
+        }
+
+        private void loginButton_Clicked(object sender, EventArgs e)
+        {
+            bool isEmailEmpty = string.IsNullOrEmpty(emailEntry.Text);
+            bool isPasswortEmpty = string.IsNullOrEmpty(passwordEntry.Text);
+
+            if(isEmailEmpty || isPasswortEmpty)
+            {
+
+            }
+            else
+            {
+                Navigation.PushAsync(new HomePage());
+            }
+        }
+    }
+}
---
+++ TravelRecordApp/MapPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TravelRecordApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MapPage : ContentPage
+    {
+        public MapPage()
+        {
+            InitializeComponent();
+        }
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            GetLocation();
+        }
+
+        private async void GetLocation()
+        {
+            var status = await CheckAndRequistLocationPermission();
+
+            if (status == PermissionStatus.Granted)
+            {
+                var location = await Geolocation.GetLocationAsync();
+                locationsMap.IsShowingUser = true;
+            }
+        }
+
+        private async Task<PermissionStatus> CheckAndRequistLocationPermission()
+        {
+            var status = await Permissions.CheckStatusAsync<Permissions.LocationWhenInUse>();
+            if (status == PermissionStatus.Granted)
+                return status;
+
+            if (status == PermissionStatus.Denied && DeviceInfo.Platform == DevicePlatform.Android)
+                return status;
+
+            status = await Permissions.RequestAsync<Permissions.LocationWhenInUse>();
+            return status;
+        }
+    }
+}
